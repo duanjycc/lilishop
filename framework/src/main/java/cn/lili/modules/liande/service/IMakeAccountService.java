42a674a57d387e6fb1432b4161fdb652a24c510c
denglei <denglei@123>
2022-08-07 11:34:40 +0800

    做单

--- framework/src/main/java/cn/lili/modules/liande/service/IMakeAccountService.java
+++ framework/src/main/java/cn/lili/modules/liande/service/IMakeAccountService.java
@@ -1,7 +1,11 @@
 
 package cn.lili.modules.liande.service;
 
+import cn.lili.common.security.context.UserContext;
 import cn.lili.modules.liande.entity.dos.MakeAccount;
+import cn.lili.modules.liande.entity.dto.MakeAccountDTO;
+import cn.lili.modules.liande.entity.vos.MakeAccountVOS;
+import cn.lili.modules.member.entity.dos.Member;
 import com.baomidou.mybatisplus.extension.service.IService;
 
 /**
@@ -14,4 +18,6 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface IMakeAccountService extends IService<MakeAccount> {
 
+
+    MakeAccountVOS makeAccount(MakeAccountDTO makeAccountDTO);
 }

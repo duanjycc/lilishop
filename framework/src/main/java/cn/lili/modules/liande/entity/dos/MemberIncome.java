c88db5e6fcc27e320c86cb4a4eb73c6d265ab8b5
only <Mima@135>
2022-08-13 00:47:28 +0800

    功能开发

--- framework/src/main/java/cn/lili/modules/liande/entity/dos/MemberIncome.java
+++ framework/src/main/java/cn/lili/modules/liande/entity/dos/MemberIncome.java
@@ -10,6 +10,7 @@ import java.time.LocalDateTime;
 import java.util.Date;
 
 import com.baomidou.mybatisplus.annotation.TableField;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
@@ -59,6 +60,7 @@ public class MemberIncome extends Model<MemberIncome> {
      * 创建时间
      */
     @ApiModelProperty(value = "创建时间")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date creationTime;
 
 

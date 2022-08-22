c112af9519f15693630fa6f91fe95d518031c69b
only <Mima@135>
2022-08-22 16:44:09 +0800

    功能开发

---
+++ buyer-api/src/main/java/cn/lili/controller/liande/ScoreAcquisitionController.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright © 2022- ~ hc R&D 电信支撑部/产品研发中心 All rights reserved.
+ */
+package cn.lili.controller.liande;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 积分获取明细 前端控制器
+ * </p>
+ *
+ * @author zh
+ * @since 2022-08-22
+ */
+@RestController
+@RequestMapping("/ui/scoreAcquisition")
+public class ScoreAcquisitionController {
+
+}
+
---
+++ framework/src/main/java/cn/lili/modules/liande/serviceimpl/ScoreAcquisitionServiceImpl.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright © 2022- ~ hc R&D 电信支撑部/产品研发中心 All rights reserved.
+ */
+package cn.lili.modules.liande.serviceimpl;
+
+import cn.lili.modules.liande.entity.dos.ScoreAcquisition;
+import cn.lili.modules.liande.mapper.ScoreAcquisitionMapper;
+import cn.lili.modules.liande.service.IScoreAcquisitionService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * <p>
+ * 积分获取明细 服务实现类
+ * </p>
+ *
+ * @author zh
+ * @since 2022-08-22
+ */
+@Slf4j
+@Service
+public class ScoreAcquisitionServiceImpl extends ServiceImpl<ScoreAcquisitionMapper, ScoreAcquisition> implements IScoreAcquisitionService {
+
+}

c480008782647af4643bfdac6eb7cd73123362cf
denglei <denglei@123>
2022-08-30 10:34:25 +0800

    修改做当父服务商获取明细

--- buyer-api/src/main/java/cn/lili/controller/liande/RegionalPromotionController.java
+++ buyer-api/src/main/java/cn/lili/controller/liande/RegionalPromotionController.java
@@ -1,5 +1,6 @@
 package cn.lili.controller.liande;
 
+import cn.lili.common.enums.ResultUtil;
 import cn.lili.common.vo.PageVO;
 import cn.lili.common.vo.ResultMessage;
 import cn.lili.modules.liande.entity.dos.RegionalPromotion;
@@ -22,9 +23,9 @@ public class RegionalPromotionController {
 
     @ApiOperation(value = "区域推广员列表")
     @GetMapping("/listOfPromoters")
-    public IPage<RegionalPromotion> listOfPromoters(PageVO page) {
+    public  ResultMessage<IPage<RegionalPromotion>> listOfPromoters(PageVO page) {
 
-        return regionalPromotionService.listOfPromoters(page);
+        return ResultUtil.data(regionalPromotionService.listOfPromoters(page));
     }
 
     @ApiOperation(value = "区域推广员添加")

246941bb4929273bafc0df0e2df2b06b12e71641
duanjycc <312311597@qq.com>
2022-11-18 21:44:07 +0800

    1

--- buyer-api/src/main/java/cn/lili/controller/liande/HomeSsdController.java
+++ buyer-api/src/main/java/cn/lili/controller/liande/HomeSsdController.java
@@ -4,12 +4,20 @@ import cn.lili.common.enums.ResultUtil;
 import cn.lili.common.vo.ResultMessage;
 import cn.lili.modules.liande.entity.vo.HomeSsdCountVO;
 import cn.lili.modules.liande.service.IHomeSsdCountService;
+import cn.lili.modules.statistics.entity.vo.WssdHisDataVO;
+import cn.lili.modules.statistics.service.StoreFlowStatisticsService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Comparator;
+import java.util.List;
 
 /**
  * 首页ssd统计
@@ -22,11 +30,33 @@ public class HomeSsdController {
     @Autowired
     private IHomeSsdCountService IHomeSsdCountService;
 
+    @Autowired
+    private StoreFlowStatisticsService storeFlowStatisticsService;
 
     @ApiOperation(value = "首页ssd统计")
     @GetMapping
     public ResultMessage<HomeSsdCountVO> count() {
         return ResultUtil.data(IHomeSsdCountService.count());
+    }
+
+    @ApiOperation(value = "行情走势")
+    @GetMapping("/getSsdPriceTopData")
+    public ResultMessage<List<WssdHisDataVO>> getSsdPriceTopData() {
+        QueryWrapper queryWrapper = Wrappers.query(); queryWrapper.orderByDesc("date_id");
+        //查询最近一个月记录
+        Page page = new Page<WssdHisDataVO>(1, 30);
+
+        List<WssdHisDataVO> topDataList = storeFlowStatisticsService.getSsdPriceTopData(page, queryWrapper);
+        if (topDataList != null) {
+            topDataList.sort(new Comparator<WssdHisDataVO>() {
+                @Override
+                public int compare(WssdHisDataVO o1, WssdHisDataVO o2) {
+                    return o1.getCreateTime().compareTo(o2.getCreateTime());
+                }
+            });
+        }
+
+        return ResultUtil.data(topDataList);
     }
 
 }
